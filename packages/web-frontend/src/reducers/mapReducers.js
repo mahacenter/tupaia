f8b6ecec507461a96d367c74b481690e2d0138a7
Rohan Port <rohan@beyondessential.com.au>
2020-03-24 18:16:05 +1100

    239: Added handing for 'shaded-spectrum' measure type

--- packages/web-frontend/src/reducers/mapReducers.js
+++ packages/web-frontend/src/reducers/mapReducers.js
@@ -35,6 +35,7 @@ import {
   getMeasureDisplayInfo,
   calculateRadiusScaleFactor,
   MEASURE_TYPE_SHADING,
+  MEASURE_TYPE_SHADED_SPECTRUM,
 } from '../utils/measures';
 
 import { initialOrgUnit } from '../defaults';
@@ -293,7 +294,10 @@ export const selectHasPolygonMeasure = createSelector(
   (stateMeasureInfo = {}) => {
     return (
       stateMeasureInfo.measureOptions &&
-      stateMeasureInfo.measureOptions.some(option => option.type === MEASURE_TYPE_SHADING)
+      stateMeasureInfo.measureOptions.some(
+        option =>
+          option.type === MEASURE_TYPE_SHADING || option.type === MEASURE_TYPE_SHADED_SPECTRUM,
+      )
     );
   },
 );
