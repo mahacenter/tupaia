dbe046b82447d274fefb77de9d364283ab5d5583
Rohan Port <rohan@beyondessential.com.au>
2020-03-24 18:23:41 +1100

    239: Migration to add 'Active cases by state' COVID map overlay

---
+++ packages/database/src/migrations/20200323051458-AddNumberActiveCasesCOVIDAUMapOverlay.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.runSql(`
+  INSERT INTO "mapOverlay" (	
+    "id",	
+    "name",	
+    "groupName",	
+    "userGroup",	
+    "dataElementCode",	
+    "displayType",	
+    "customColors",	
+    "isDataRegional",	
+    "values",	
+    "hideFromMenu",	
+    "hideFromPopup",	
+    "hideFromLegend",	
+    "linkedMeasures",	
+    "sortOrder",	
+    "measureBuilderConfig",	
+    "measureBuilder",	
+    "presentationOptions",	
+    "countryCodes")	
+     
+    VALUES (
+    'COVID_AU_State_Number_Active_Cases',
+    'Number of active cases by state',
+    'COVID-19 Australia',
+    'Public',
+    'value',
+    'shaded-spectrum',
+    '',
+    false,
+    '[{"color": "blue", "value": "other"}, {"color": "grey", "value": null}]',
+    false,
+    false,
+    false,
+    NULL,
+    '0',
+    '{"dataSourceEntityType":"region","aggregationEntityType":"region","pipeline":{"query":{"endpoint":"analytics","dataElementCodes":["dailysurvey003","dailysurvey004","dailysurvey005"]},"functions":[{"function":"select","arguments":["dailysurvey003","value"]},{"function":"subtract","arguments":["value","dailysurvey004"]},{"function":"subtract","arguments":["value","dailysurvey005"]}]}}',
+    'functionalPipeline',
+    '{"scaleType": "performanceDesc"}',
+    '{AU}');
+  `);
+};
+
+exports.down = function(db) {
+  return db.runSql(
+    `	
+    DELETE FROM "mapOverlay" WHERE "id" = 'STRIVE_FIS_Village_Number_Reported_Cases_In_Week';	
+  `,
+  );
+};
+
+exports._meta = {
+  version: 1,
+};
