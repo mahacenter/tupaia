427d6bb760303e708d9b5120f4aa2101d06ca830
Edwin <edmofro@gmail.com>
2020-08-04 17:01:02 +1200

    Build ui-components serially

--- scripts/bash/buildInternalDependencies.sh
+++ scripts/bash/buildInternalDependencies.sh
@@ -30,21 +30,36 @@ done
 [[ $watch = "true" ]] && build_args="--watch" || build_args=""
 [[ $watch = "true" ]] && build_ts_args="--watch" || build_ts_args=""
 
-concurrent_build_commands=""
+# ui-components building is quite resource intensive, so we avoid parallelising
+PACKAGES_FOR_SERIAL_BUILD=("ui-components")
+
+concurrent_build_commands=()
+serial_build_commands=()
 
 # Build dependencies
 for PACKAGE in $(${DIR}/getInternalDependencies.sh); do
-  concurrent_build_commands="${concurrent_build_commands} \"yarn workspace @tupaia/${PACKAGE} build $build_args\""
+  build_command="yarn workspace @tupaia/${PACKAGE} build $build_args"
+  if [[ " ${PACKAGES_FOR_SERIAL_BUILD[@]} " =~ " ${PACKAGE} " ]]; then
+    serial_build_commands+=("${build_command}")
+  else
+    concurrent_build_commands+=("\"${build_command}\"")
+  fi
 done
 
 # Build types
 if [ $with_types == "true" ]; then
   for PACKAGE in $(${DIR}/getTypedInternalDependencies.sh); do
-    concurrent_build_commands="${concurrent_build_commands} \"yarn workspace @tupaia/${PACKAGE} build:ts $build_ts_args\""
+    concurrent_build_commands+=("\"yarn workspace @tupaia/${PACKAGE} build:ts $build_ts_args\"")
   done
 fi
 
-echo yarn concurrently ${concurrent_build_commands}
-eval "yarn concurrently ${concurrent_build_commands}"
 echo "Concurrently building internal dependencies"
+echo "yarn concurrently ${concurrent_build_commands[@]}"
+eval "yarn concurrently ${concurrent_build_commands[@]}"
+
 echo "Serially building resource intensive internal dependencies"
+for build_command in "${serial_build_commands[@]}"; do
+  echo ${build_command}
+  ${build_command}
+done
+
