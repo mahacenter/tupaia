6f49e8899a3d609b2b09c2558c7e561be099acc6
Edwin <edmofro@gmail.com>
2020-08-04 17:08:36 +1200

    Build ui-components in serial

--- scripts/bash/buildInternalDependencies.sh
+++ scripts/bash/buildInternalDependencies.sh
@@ -2,7 +2,13 @@
 DIR=`dirname "$0"`
 concurrent_build_commands=""
 for PACKAGE in $(${DIR}/getInternalDependencies.sh); do
-  concurrent_build_commands="${concurrent_build_commands} \"yarn workspace @tupaia/${PACKAGE} build $1\"" # $1 may pass in --watch
+  if [[ ${PACKAGE} != "ui-components" ]]; then # skip ui-components, as it is too resource-intensive to run in parallel
+    concurrent_build_commands="${concurrent_build_commands} \"yarn workspace @tupaia/${PACKAGE} build $1\"" # $1 may pass in --watch
+  fi
 done
-echo yarn concurrently ${concurrent_build_commands}
+echo "Building internal dependencies in parallel"
+echo "yarn concurrently ${concurrent_build_commands}"
 eval "yarn concurrently ${concurrent_build_commands}"
+
+echo "Building ui-components in serial"
+yarn workspace @tupaia/ui-components build
