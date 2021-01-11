4ff82c7a27af6285235d18f8483593841186f36f
Edwin <edmofro@gmail.com>
2021-01-11 16:51:49 +1300

    Fetch from ElasticSearch

--- packages/data-api/src/fetchData.js
+++ packages/data-api/src/fetchData.js
@@ -2,57 +2,87 @@
  * Tupaia
  * Copyright (c) 2017 - 2020 Beyond Essential Systems Pty Ltd
  */
-import { utcMoment } from '@tupaia/utils';
+import { fetchWithTimeout } from '@tupaia/utils';
 
-import { SqlQuery } from './SqlQuery';
+const generateBaseQuery = ({ dataElementCodes, organisationUnitCodes, startDate, endDate }) => {
+  const matchClauses = [];
 
-const generateBaseSqlQuery = ({ dataElementCodes, organisationUnitCodes, startDate, endDate }) => {
-  const sqlQuery = new SqlQuery(
-    `
-    SELECT
-      survey_response_id as "surveyResponseId",
-      date,
-      entity_code as "entityCode",
-      entity_name as "entityName",
-      data_element_code as "dataElementCode",
-      type,
-      value
-    FROM
-      analytics
-    WHERE
-      data_element_code IN ${SqlQuery.parameteriseArray(dataElementCodes)}
-    AND
-      entity_code IN ${SqlQuery.parameteriseArray(organisationUnitCodes)}
-  `,
-    [...dataElementCodes, ...organisationUnitCodes],
-  );
+  matchClauses.push({
+    terms: {
+      data_element_code: dataElementCodes,
+    },
+  });
 
-  // Add start and end date, which are inclusive
-  if (startDate) {
-    sqlQuery.addClause(`AND date >= ?`, [utcMoment(startDate).startOf('day').toISOString()]);
-  }
-  if (endDate) {
-    sqlQuery.addClause(`AND date <= ?`, [utcMoment(endDate).endOf('day').toISOString()]);
+  matchClauses.push({
+    terms: {
+      entity_code: organisationUnitCodes.map(c => c.replace("'", '')),
+    },
+  });
+
+  if (startDate && endDate) {
+    matchClauses.push({
+      range: {
+        date_of_data: {
+          gte: `${startDate} 00:00:00`,
+          lte: `${endDate} 23:59:59`,
+        },
+      },
+    });
   }
 
-  sqlQuery.orderBy('date');
+  const query = {
+    size: 10000,
+    sort: [{ date_of_data: { order: 'asc' } }],
+    query: { bool: { must: matchClauses } },
+  };
 
-  sqlQuery.logQuery();
-
-  return sqlQuery;
+  return query;
 };
 
 export async function fetchEventData(database, options) {
-  const sqlQuery = generateBaseSqlQuery(options);
-  const { surveyCode, eventId } = options;
-  sqlQuery.addClause(`AND survey.code = ?`, [surveyCode]);
-  if (eventId) {
-    sqlQuery.addClause(`AND survey_response.id = ?`, [eventId]);
-  }
-  return sqlQuery.executeOnDatabase(database);
+  return [];
+}
+
+async function fetchPage(query) {
+  const s = Date.now();
+  const response = await fetchWithTimeout(
+    'https://search-tupaia-test-s3mzchqqwhorlsgw7stw7kpwc4.ap-southeast-2.es.amazonaws.com/analytics_test_mon_2/_search',
+    {
+      method: 'POST',
+      body: JSON.stringify(query),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+  console.log('request took', Date.now() - s);
+  const d = Date.now();
+  const r = await response.json();
+  console.log('json took', Date.now() - d);
+  return r;
 }
 
 export async function fetchAnalyticData(database, options) {
-  const sqlQuery = generateBaseSqlQuery(options);
-  return sqlQuery.executeOnDatabase(database);
+  const baseQuery = generateBaseQuery(options);
+  const results = [];
+  let checkForNextPage = true;
+  while (checkForNextPage) {
+    const s = Date.now();
+    const lastResult = results.length > 0 ? results[results.length - 1] : null;
+    const query = lastResult ? { ...baseQuery, search_after: lastResult.sort } : baseQuery;
+    // response format: { ..., hits: { total: { value: 100, relation: 'gte' }, hits: [{...}, {...}] } }
+    const { hits, took } = await fetchPage(query);
+    results.push(...hits.hits);
+    checkForNextPage = hits.hits.length >= 9999;
+    console.log('page took', Date.now() - s, 'number results', hits.hits.length, 'es took', took);
+  }
+  return results.map(({ _source: analytic }) => ({
+    surveyResponseId: analytic.survey_response_id,
+    date: analytic.date_of_data,
+    entityCode: analytic.entity_code,
+    entityName: analytic.entity_name,
+    dataElementCode: analytic.data_element_code,
+    type: analytic.type,
+    value: analytic.value,
+  }));
 }
